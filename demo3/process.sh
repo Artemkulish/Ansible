1b0e3fcf71792069f04e97a23618ca6fadd51b45
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-04-14 22:44:43 +0300

    add process.sh

---
+++ demo3/process.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+array=$( ls /home/env/)
+var1=$( sed -n 1p file.txt)
+var2=$( sed -n 2p file.txt)
+var3=$( sed -n 3p file.txt)
+
+for i in ${array[@]}
+
+do
+ip=$( cat /home/env/$i | grep jdbc | awk -F"/" '{ print $3}')
+pass=$( cat /home/env/$i | grep 'DATABASE_PASSWORD\|SQL_PASSWORD' |  awk -F"=" '{ print $2}')
+user=$( cat /home/env/$i | grep 'DATABASE_USERNAME\|SQL_USERNAME' |  awk -F"=" '{ print $2}')
+
+sed -i  "s/${ip}/${var1}/g; s/${pass}/${var2}/; s/${user}/${var3}/" /home/env/$i
+done
+
+rm file.txt
+
