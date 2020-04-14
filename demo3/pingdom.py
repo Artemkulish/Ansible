64f3c66cfd3ed7574840caf75c5035269d141f23
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-04-14 19:53:46 +0300

    Create pingdom.py

---
+++ demo3/pingdom.py
@@ -0,0 +1,15 @@
+import requests
+
+token = "$PINGDOM_TOKEN"
+hed = {'Authorization': 'Bearer ' + token}
+url = "https://api.pingdom.com/api/3.1/checks"
+host = "34.69.1.126"
+team_id = "482197"
+port = [ "8080", "8081", "8082", "8083", "8084" ]
+
+
+i = 0
+while i < 5:
+    request = {	"name": port[i], "host": host, "port": port[i], "type": "tcp", "resolution": "1", "sendnotificationwhendown": "1", "teamids": team_id } 
+    response = requests.post(url, data=request, headers=hed)
+    i += 1
